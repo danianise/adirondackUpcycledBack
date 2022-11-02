fb1665dd166ebc69faf944e50735eb22ee6d8edb
danianise <danianisehoey@gmail.com>
2022-11-01 21:40:58 -0400

    First commit: setting up Django backend

---
+++ adirondack_upcycled_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdirondackUpcycledAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'adirondack_upcycled_app'
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE adirondack_upcycled;
+CREATE USER adirondack_upcycled_admin WITH PASSWORD '4dminPassw0rd!';
+GRANT ALL PRIVILEGES ON DATABASE adirondack_upcycled TO adirondack_upcycled_admin;
