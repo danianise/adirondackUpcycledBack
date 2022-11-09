96bcdd703b031ea5fca5a685b011e34d73f5d7f8
danianise <danianisehoey@gmail.com>
2022-11-09 12:31:02 -0500

    Explicitly added ID to Category model for later use

--- adirondack_upcycled_app/models.py
+++ adirondack_upcycled_app/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    id = models.BigAutoField(primary_key=True)
 
     def __str__(self):
-        return self.name
+        return '{}. {}'.format(self.id, self.name)
 
 class Listing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categories', default=1)
