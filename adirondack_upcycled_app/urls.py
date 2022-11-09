b79cf93d181365b0bab78ddf495c6a485e018716
danianise <danianisehoey@gmail.com>
2022-11-08 22:28:34 -0500

    Configuring ImageField rendering for frontend

--- adirondack_upcycled_app/urls.py
+++ adirondack_upcycled_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('listings/', views.ListingView.as_view(), name='listings_all'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
