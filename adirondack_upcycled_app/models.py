8fab0321f7ca404a025b122bf261e48aaa5ee11b
danianise <danianisehoey@gmail.com>
2022-11-22 22:07:58 -0500

    added hrefName field for categories

--- adirondack_upcycled_app/models.py
+++ adirondack_upcycled_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
     id = models.BigAutoField(primary_key=True)
+    hrefName = models.CharField(max_length=50, default="")
 
     def __str__(self):
         return '{}. {}'.format(self.id, self.name)
