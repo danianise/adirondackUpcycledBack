e87dd6954a064f60c684a23757b3a812b3b82d7a
danianise <danianisehoey@gmail.com>
2023-01-25 23:08:01 -0500

    Added quantity option in Listing model

--- adirondack_upcycled_app/models.py
+++ adirondack_upcycled_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from model_utils import Choices
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -19,6 +20,8 @@ class Listing(models.Model):
     photo5 = models.ImageField(upload_to='images/', null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, default=9999.99)
     description = models.TextField()
+    QUANTITY = Choices(1, 2, 3, 4, 5)
+    quantity = models.IntegerField(choices=QUANTITY, default=1)
     available = models.BooleanField(default=True)
 
     def __str__(self):
