794df2e903af4e0fbfdbd26b04a7afef55221e04
danianise <danianisehoey@gmail.com>
2023-02-09 15:36:46 -0500

    Settings.py changes

--- adirondack_upcycled/settings.py
+++ adirondack_upcycled/settings.py
@@ -77,8 +77,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # 'django.template.context_processors.media'
-                'django.core.context_processors.media',
+                'django.template.context_processors.media'
+                # 'django.core.context_processors.media',
             ],
         },
     },
