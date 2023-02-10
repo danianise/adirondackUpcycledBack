44dad8dffac4656dc9226d013718cc40c9d9f8b0
danianise <danianisehoey@gmail.com>
2023-02-09 21:12:26 -0500

    Changes

--- adirondack_upcycled/settings.py
+++ adirondack_upcycled/settings.py
@@ -147,7 +147,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # MEDIA_URL = '/media/'
-MEDIA_ROOT = ''
+# MEDIA_ROOT = ''
 MEDIA_URL = ''
--- adirondack_upcycled/wsgi.py
+++ adirondack_upcycled/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from adirondack_upcycled import BASE_DIR
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adirondack_upcycled.settings')
 
