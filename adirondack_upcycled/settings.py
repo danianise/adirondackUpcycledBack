5a988cbe229b1a96b374227cf0fa719b5b658485
danianise <danianisehoey@gmail.com>
2023-02-11 20:58:26 -0500

    nginx retry

--- adirondack_upcycled/settings.py
+++ adirondack_upcycled/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'web/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'web/static')]
 # STATIC_ROOT=os.path.join(BASE_DIR, "static/")
 STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 
